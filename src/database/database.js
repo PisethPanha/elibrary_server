90a14e7088abd4ab5ef780c583f7f064acfe0484
PisethPanha <yornpisethpanha58@gmail.com>
2025-02-28 23:48:29 +0700

    chang database

--- src/database/database.js
+++ src/database/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2')
 
 
     const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "khmer_elib",
+        host: "bho9cystmcal7phrxsmp-mysql.services.clever-cloud.com",
+        user: "uhcjtzhelk4m7qyr",
+        password: "RhzRrbzNOkSe6LL29hzy",
+        database: "bho9cystmcal7phrxsmp",
     })
     module.exports = {connection}
 
