2c1792558bf3c93506362a9f27860a68d07b581e
PisethPanha <100303620+PisethPanha@users.noreply.github.com>
2025-03-03 22:24:45 +0700

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2')
 
 
     const connection = mysql.createConnection({
-        host: "bfeetst4vbxy2lvuhbp1-mysql.services.clever-cloud.com" ,
-        user: "uhcjtzhelk4m7qyr",
-        password: "RhzRrbzNOkSe6LL29hzy",
-        database: "bfeetst4vbxy2lvuhbp1-mysql.services.clever-cloud.com",
-        port: "20417"
+        host: "bqpchigzuhyeltchb2ff-mysql.services.clever-cloud.com" ,
+        user: "uazqoodj3xoeyoyl",
+        password: "hc2YrowbYEZEZp0s5wv",
+        database: "bqpchigzuhyeltchb2ff",
+        port: "20600"
     })
     module.exports = {connection}
 
